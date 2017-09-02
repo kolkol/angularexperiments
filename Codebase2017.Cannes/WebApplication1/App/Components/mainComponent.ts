850d9f981b04fc705256ef2cdfd14648b707d27d
Johan Schedin Jigland <kolkol2k@hotmail.com>
2017-09-02 21:25:33 +0700

    Some angular stuff added, builds but doesnt do anything

---
+++ Codebase2017.Cannes/WebApplication1/App/Components/mainComponent.ts
@@ -0,0 +1,15 @@
+﻿import { Component, OnInit } from '@angular/core';  
+
+@Component({
+    selector: 'main-component',
+    template: '<h3>HEre is a hardcoded template</h3>',
+    providers: []
+})
+export class MainComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+        console.log("Ran on init")
+    }
+}
---
+++ Codebase2017.Cannes/WebApplication1/App/mainModule.ts
@@ -0,0 +1,18 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MainComponent } from './Components/mainComponent';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+@NgModule({
+    declarations: [
+        MainComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [MainComponent]
+})
+export class MainModule { }
