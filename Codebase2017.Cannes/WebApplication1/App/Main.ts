9d1fe3381fd1b87a5edaa930444a0297f63901eb
Johan Schedin Jigland <kolkol2k@hotmail.com>
2017-09-03 11:03:34 +0700

    First working hello world edition

--- Codebase2017.Cannes/WebApplication1/App/Main.ts
+++ Codebase2017.Cannes/WebApplication1/App/Main.ts
@@ -1,4 +1,4 @@
-﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { MainModule } from './mainModule';
 
 platformBrowserDynamic().bootstrapModule(MainModule);
--- Codebase2017.Cannes/WebApplication1/App/mainModule.ts
+++ Codebase2017.Cannes/WebApplication1/App/mainModule.ts
@@ -1,4 +1,4 @@
-﻿import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './Components/mainComponent';
 import { FormsModule } from '@angular/forms';
